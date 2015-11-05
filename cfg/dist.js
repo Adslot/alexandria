893d1dd0f5acb51bb1637d036c5e5a600cb04745
Cameron Chamberlain <git@camjc.com>
2015-11-05 15:22:34 +1100

    Output UMD modules
    - Each separate component is exported as a module.
    - There is also a common module for duplicated code
      (like the style loader). This should stay small.
    - External dependencies (React, Lodash) are removed
      from the bundle and are assumed to be pulled in separately.
    - Remove `npm run copy` from `npm run dist` since we don't
      need to distribute an index.html or favicon.
    - Add note to the README about profiling the build.

--- cfg/dist.js
+++ cfg/dist.js
@@ -1,21 +1,56 @@
+const _ = require('lodash');
+const baseConfig = require('./base');
 const path = require('path');
 const webpack = require('webpack');
-const _ = require('lodash');
 
-const baseConfig = require('./base');
+const alexandriaPath = path.resolve(__dirname, '../src/components/alexandria');
 
-const config = _.merge({
-  entry: path.join(__dirname, '../src/components/run'),
+const config = _.merge(baseConfig, {
   cache: false,
   devtool: 'sourcemap',
+  externals: {
+    lodash: {
+      root: '_',
+      commonjs2: 'lodash',
+      commonjs: 'lodash',
+      amd: 'lodash',
+    },
+    react: {
+      root: 'react',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react',
+    },
+    "react-dom": {
+      root: 'ReactDOM',
+      commonjs2: 'ReactDOM',
+      commonjs: 'ReactDOM',
+      amd: 'ReactDOM',
+    },
+  },
+  entry: {
+    slicey: path.join(alexandriaPath, '/SliceyComponent.js'),
+    marker: path.join(alexandriaPath, '/slicey/MarkerComponent.js'),
+    donut: path.join(alexandriaPath, '/slicey/DonutComponent.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'alexandria-[name].js',
+    libraryTarget: 'umd',
+  },
   plugins: [
+    new webpack.optimize.CommonsChunkPlugin('alexandria-common.js'),
     new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+      },
+    }),
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.AggressiveMergingPlugin(),
     new webpack.NoErrorsPlugin(),
   ],
-}, baseConfig);
+});
 
 config.module.loaders.push({
   test: /\.(js|jsx)$/,
