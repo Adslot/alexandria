43876f63ae8407cd314c8449eae7288dfc655d56
Cameron Chamberlain <git@camjc.com>
2015-11-20 18:22:48 +1100

    Add Search component
    - Add Search Component.
    - Change all sass-lint rules to hard errors instead of warnings.
    - Only warn about no-url-protocols, no-vendor-prefixes since we need to do them.
    - Remove unnecessary source map generation from distribution file.
    - Run sass-lint as part of the TravisCI job and when running `npm run test`.
    - Remove `npm run copy` since we aren't using it.
    - Clean up the development page's styles, with nicer spacing between components.
    - Start using svg icons as background-images so that consumers can override them.
    - Start using our color palette throughout Alexandria.
    - Fix linting issue in shallowRenderHelper.
    - Update dependencies.

--- src/components/alexandria/components-main.js
+++ src/components/alexandria/components-main.js
@@ -1,7 +1,9 @@
 // Export all the components that are consumable.
 
+import Search from 'components/alexandria/SearchComponent';
 import Slicey from 'components/alexandria/SliceyComponent';
 
 module.exports = {
+  Search,
   Slicey,
 };
