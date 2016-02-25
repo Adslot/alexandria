66bc2b3d26359d3c62ec8bacaf8b85934a7a7acf
Vincent Teo <vincent.teo@adslot.com>
2016-02-25 17:49:23 +1100

    Update: SVG Symbol support for Empty component

--- src/components/alexandria/EmptyComponent.js
+++ src/components/alexandria/EmptyComponent.js
@@ -1,13 +1,19 @@
 import _ from 'lodash';
 import React, { PropTypes } from 'react';
+import SvgSymbol from 'components/alexandria/SvgSymbolComponent';
 
 require('styles/alexandria/Empty.scss');
 
-const EmptyComponent = ({ collection, icon, text }) => {
+const EmptyComponent = ({ collection, icon, svgSymbol, text }) => {
   if (_.isEmpty(collection)) {
     return (
       <div className="empty-component">
-        <img className="empty-component-icon" src={icon} />
+        {svgSymbol === undefined ?
+          <img className="empty-component-icon" src={icon} /> :
+          <div className="empty-component-svg-symbol">
+            <SvgSymbol classSuffixes={svgSymbol.classSuffixes} href={svgSymbol.href} />
+          </div>
+        }
         <div className="empty-component-text">{text}</div>
       </div>
     );
@@ -21,6 +27,7 @@ EmptyComponent.displayName = 'AlexandriaEmptyComponent';
 EmptyComponent.propTypes = {
   collection: PropTypes.any,
   icon: PropTypes.string,
+  svgSymbol: PropTypes.shape(SvgSymbol.propTypes),
   text: PropTypes.string,
 };
 EmptyComponent.defaultProps = {
